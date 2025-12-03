29c0b04cd03854850aa2405546902ec5ecbc8d33
OlexandrOnokalo <Olexandr.onokalo@gmail.com>
2025-12-03 17:47:03 +0200

    08 Events LAB

---
+++ 08_Events/HW01/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const htmlBtn = document.getElementById('htmlBtn');
+    htmlBtn.addEventListener("click", pressHtmlBtn);
+
+    const CSSBtn = document.getElementById('CSSBtn');
+    CSSBtn.addEventListener("click", pressCSSBtn);
+
+    const JSBtn = document.getElementById('JSBtn');
+    JSBtn.addEventListener("click", pressJSBtn);
+});
+
+
+
+
+
+
+
+
